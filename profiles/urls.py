88a5d18c84c5616ed1a9df0fb865be4ff224234b
Gabe Getzie <ggetzie@gmail.com>
2008-10-10 11:35:35 -0400

    why was I ignoring urls.*?

---
+++ profiles/urls.py
@@ -0,0 +1,16 @@
+# This url file is included in tcd.items.urls with the prefix '^users/'
+
+from django.conf.urls.defaults import *
+from django.contrib.auth.views import logout
+
+
+
+
+
+urlpatterns = patterns('',
+                       (r'^login/', 'tcd.profiles.views.login'),
+                       (r'^logout/', logout, {'next_page': "/"}),                       
+                       (r'^register/$', 'tcd.profiles.views.register'),
+                       (r'^(?P<value>[A-Za-z\d]+)/', include('tcd.profiles.user_urls')),
+                       )
+
