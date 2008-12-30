66ee692acb87df798c0cc8bea7cd5564663c4779
gabe <ggetzie@gmail.com>
2008-12-29 20:05:12 -0500

    vinepics.html

--- urls.py
+++ urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
                        (r'^tcdsetup.html$', 'django.views.generic.simple.direct_to_template', {'template': 'tcdsetup.html'}),
+                       (r'^vinepics.html$', 'django.views.generic.simple.direct_to_template', {'template': 'vinepics.html'}),
                        (r'^admin/', include('django.contrib.admin.urls')),
                        (r'', include('tcd.items.urls')),                       
 )
