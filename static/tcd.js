8358a6b6385ffded344014c5e4ddb6ad70e9fab9
Gabe Getzie <ggetzie@gmail.com>
2009-01-23 06:45:04 -0500

    added collapse

--- static/tcd.js
+++ static/tcd.js
@@ -44,3 +44,32 @@ function addVotes(xml) {
 		    $("pvotes",xml).text() +  pvotefor + $("plaintiff",xml).text() + "<br />" + 
 		    $("dvotes",xml).text() + dvotefor +  $("defendant",xml).text() + "</p>");
 }
+
+function collapse(div_id, mode) {
+    var div = $(div_id);
+    var margin = div.css("margin-left");
+    
+    while ( div.next().css("margin-left") > margin ) {
+	
+	if (mode == 0) {
+	    div.next().css("display", "none");
+	} else if (mode == 1) {
+	    div.next().css("display", "");
+	}
+
+	div = div.next();
+    }
+
+    collapseID = div_id.replace(/comment_div/, "collapse");
+
+    if (mode == 0) {
+	$(collapseID).html("[+]");
+	$(collapseID).attr("href","javascript:collapse('" + div_id + "', 1);");
+    } else if (mode == 1) {
+	$(collapseID).html("[-]");
+	$(collapseID).attr("href","javascript:collapse('" + div_id + "', 0);");
+    } else {
+	alert("Invalid mode!");
+    };
+	
+}
