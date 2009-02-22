48d89278f1657a4d2cdbce06dcd37c9e651695e3
Gabe Getzie <ggetzie@gmail.com>
2009-02-22 11:39:49 -0500

    no longer edit topic url

--- items/views.py
+++ items/views.py
@@ -30,7 +30,8 @@ def comments(request, topic_id, page=1):
     has_next = True
     top = get_object_or_404(Topic, pk=topic_id)
     comments = top.comment_set.filter(is_msg=False, 
-                                      arg_proper=False)
+                                      arg_proper=False,
+                                      needs_review=False)
     first_c = comments.filter(is_first=True)
     rest_c = build_list(comments.filter(is_first=False).order_by('-pub_date'), 0)
     form_comment = CommentForm()
@@ -180,31 +181,27 @@ def edit_topic(request, topic_id, page):
     top = get_object_or_404(Topic, pk=topic_id)
     c = Comment.objects.filter(topic=top, is_first=True)
     redirect = ''.join(['/users/u/', request.user.username, '/submissions/', page])    
-    if request.user == top.user and request.method == 'POST':
-        data = request.POST.copy()
-        form = tcdTopicSubmitForm(data)
-        if form.is_valid():
-            if form.cleaned_data['comment']:
-                if c:
-                    c = c[0]
-                    c.comment = form.cleaned_data['comment']
-                else:
-                    c = Comment(user=top.user,
-                                topic=top,
-                                comment = form.cleaned_data['comment'],
-                                pub_date=datetime.datetime.now(),
-                                is_first=True,
-                                parent_id=0,
-                                nesting=0)
-                c.save()
-            top.title = form.cleaned_data['title']
-            if form.cleaned_data['url']:
-                if top.url[0:4] == 'http':
-                    top.url = form.cleaned_data['url']
-                else:
-                    request.user.message_set.create(message="Can't change url of self-referential topic.")
-            top.save()
-            return HttpResponseRedirect(redirect)
+    if request.method == 'POST':
+        if request.user == top.user:
+            data = request.POST.copy()
+            form = tcdTopicSubmitForm(data)
+            if form.is_valid():
+                if form.cleaned_data['comment']:
+                    if c:
+                        c = c[0]
+                        c.comment = form.cleaned_data['comment']
+                    else:
+                        c = Comment(user=top.user,
+                                    topic=top,
+                                    comment = form.cleaned_data['comment'],
+                                    pub_date=datetime.datetime.now(),
+                                    is_first=True,
+                                    parent_id=0,
+                                    nesting=0)
+                    c.save()
+                top.title = form.cleaned_data['title']            
+                top.save()
+                return HttpResponseRedirect(redirect)
     else:
         if top.url[0:4] == 'http':
             url = top.url
@@ -220,7 +217,7 @@ def edit_topic(request, topic_id, page):
         form = tcdTopicSubmitForm(data)
     return render_to_response("items/edit_topic.html",
                               {'form': form,
-                               'username': top.user.username},
+                               'username': top.user},
                               context_instance=RequestContext(request))
 
 def challenge(request, c_id):
