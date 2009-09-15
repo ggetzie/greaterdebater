b73426829fac72870a48ff57f6e85e823c967df0
Gabe Getzie <ggetzie@gmail.com>
2009-09-15 06:20:43 -0400

    drop down menus

--- static/tcd.js
+++ static/tcd.js
@@ -4,7 +4,12 @@ $(document).ready(function() {
     	$('.message').attr('checked', $('#check_all').attr('checked'));
     });
 
+    $('.jsddm > li').bind('mouseover', jsddm_open);
+    $('.jsddm > li').bind('mouseout',  jsddm_timer)
+
 });
+
+document.onclick = jsddm_close;
     
 function displayFormComment(form_id) {
 
@@ -264,3 +269,23 @@ function delete_current_message(m_id){
 	       
 	   )
 }
+
+var timeout    = 500;
+var closetimer = 0;
+var ddmenuitem = 0;
+
+function jsddm_open()
+{  jsddm_canceltimer();
+   jsddm_close();
+   ddmenuitem = $(this).find('ul').css('visibility', 'visible');}
+
+function jsddm_close()
+{  if(ddmenuitem) ddmenuitem.css('visibility', 'hidden');}
+
+function jsddm_timer()
+{  closetimer = window.setTimeout(jsddm_close, timeout);}
+
+function jsddm_canceltimer()
+{  if(closetimer)
+   {  window.clearTimeout(closetimer);
+      closetimer = null;}}
