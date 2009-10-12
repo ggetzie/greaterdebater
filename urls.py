3303cd62ee4c0cd900fc222d152185a15526b0ec
Gabe Getzie <ggetzie@gmail.com>
2009-10-11 21:41:24 -0400

    removed about link, fixed bookmarklet, added help

--- urls.py
+++ urls.py
@@ -7,10 +7,10 @@ urlpatterns = patterns('',
                        (r'^tcdsetup.html$', 'django.views.generic.simple.direct_to_template', {'template': 'tcdsetup.html'}),
                        (r'^vinepics.html$', 'django.views.generic.simple.direct_to_template', {'template': 'vinepics.html'}),
                        (r'^admin/(.*)', admin.site.root),
-                       (r'', include('tcd.items.urls')),                       
                        (r'^about/$', 'django.views.generic.simple.direct_to_template', {'template': 'about.html'}),
                        (r'^FAQ/$', 'django.views.generic.simple.direct_to_template', {'template': 'faq.html'}),
                        (r'^help/$', 'django.views.generic.simple.direct_to_template', {'template': 'help.html'}),                       
                        (r'^tos/$', 'django.views.generic.simple.direct_to_template', {'template': 'tos.html'}),
                        (r'^privacy/$', 'django.views.generic.simple.direct_to_template', {'template': 'privacy.html'}),
+                       (r'', include('tcd.items.urls'))
 )
