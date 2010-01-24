7a57ad344525ce0c3dd0580093324eeca1b0ff20
Gabe Getzie <ggetzie@gmail.com>
2010-01-24 10:57:35 -0500

    blog 1.0

--- blog/models.py
+++ blog/models.py
@@ -9,7 +9,14 @@ class Blog(models.Model):
     author = models.ForeignKey(User)
     start_date = models.DateTimeField()
     title = models.CharField(max_length=140)
-    tagline = models.CharField(max_length=140)
+    tagline_txt = models.CharField(max_length=140, blank=True)
+    about_txt = models.TextField(blank=True)
+    about_html = models.TextField(blank=True)
+    altfeedurl = models.URLField(blank=True, verify_exists=False)
+
+    def save(self):
+        about_html = markdown(self.about_txt)
+        super(Blog, self).save()
 
 
 class Post(models.Model):
@@ -27,7 +34,7 @@ class Post(models.Model):
         verbose_name_plural = 'posts'
 
     def save(self):
-        self.html = markdown(post_txt)
+        self.html = markdown(self.txt)
         super(Post, self).save()
 
 class PostComment(Comment):
