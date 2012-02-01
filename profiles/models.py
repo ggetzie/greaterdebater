1c0172665d4e70d1420028c283852b40f8b3e882
Gabe Getzie <ggetzie@gmail.com>
2012-02-01 06:31:16 -0500

    server likes profiles to have tcd prefix for some reason

--- profiles/models.py
+++ profiles/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 
 from tcd.settings import HOSTNAME
-from base_utils import wordtime
+from tcd.base_utils import wordtime
 
 import datetime
 
